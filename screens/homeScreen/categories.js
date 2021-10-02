946f65fc414fd88b727a67c6c3c6cbcc2db67b49
ngocvu229 <vunguyen220901@gmail.com>
2021-10-02 21:13:45 +0700

    update: home screen

--- screens/homeScreen/categories.js
+++ screens/homeScreen/categories.js
@@ -44,7 +44,7 @@ const styles = StyleSheet.create({
         shadowOpacity: 0.25,
         shadowRadius: 3.84,
         elevation: 5,
-        marginBottom: 5,
+        marginBottom: 10,
     },
     image: {
         height: 60,
